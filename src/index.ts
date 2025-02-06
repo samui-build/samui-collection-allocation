1a6ec122cc79c94653d3b1dfdb5aaee5bd583c43
Bram Borggreve <beeman@beeman.nl>
2025-02-06 18:06:40 +0000

    feat: add CORS config

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,38 @@
 import { PublicKey } from '@solana/web3.js'
-import { Hono } from 'hono'
+import { Context, Hono } from 'hono'
 import { getSnapshot } from "./lib/get-snapshot";
 import { getSnapshotsForAddress } from "./lib/get-snapshots-for-address";
 import { snapshots } from "./snapshots";
+import { cors } from 'hono/cors'
+import { env } from "hono/adapter";
 
 const app = new Hono()
+
+function getEnv(c: Context) {
+  return env<{ ALLOWED_ORIGINS: string }>(c)
+}
+
+function getOrigins(c: Context): string[] {
+  const e = getEnv(c)
+  const envOrigins: string[] = getEnv(c).ALLOWED_ORIGINS?.split(';') ?? []
+
+  return envOrigins
+      .map((origin) => origin.trim())
+      .filter((origin) => origin.length > 0)
+}
+
+app.use(cors({
+  origin: (origin, c) => {
+    return getOrigins(c)?.includes(origin) ? origin : null
+  },
+}))
+
+app.get('/config', (c) => {
+  const config = {
+    ALLOWED_ORIGINS: getOrigins(c),
+  }
+  return c.json(config)
+})
 
 app.get('/', (c) => {
   return c.text('Samui Collection Allocation API')
